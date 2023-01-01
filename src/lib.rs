d43db455bf3648fc0d7c5948154d47be5bfa9fad
Laurence Qi <laurence.qi@outlook.com>
2023-01-01 21:41:13 +1100

    ye

--- src/lib.rs
+++ src/lib.rs
@@ -8,21 +8,24 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &Vec<String>) -> Result<Config, &'static str> {
-            if args.len() < 3 {
-                    return Err("not enough arguments");
-            }
-            
-            let query = args[1].clone();
-            let file_path = args[2].clone();
-			let ignore_case = env::var("IGNORE_CASE").is_ok();
-    
-            Ok(Config { query, file_path, ignore_case })
+	pub fn build(args: &Vec<String>) -> Result<Config, &'static str> {
+		if args.len() < 3 {
+			return Err("not enough arguments");
+		}
+		
+		let query = args[1].clone();
+		let file_path = args[2].clone();
+		let ignore_case = env::var("IGNORE_CASE").is_ok();
+		
+		Ok(Config { query, file_path, ignore_case })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
+	let hello = [0,1,2,3];
+
+	hello.iter();
+	let contents = fs::read_to_string(config.file_path)?;
 
 	if config.ignore_case {
 		for line in search_case_insensitive(&config.query, &contents) {
