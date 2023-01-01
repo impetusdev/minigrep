4e0d6df9a7e83b59c40b93656250f9e2209ded33
Laurence Qi <laurence.qi@outlook.com>
2023-01-01 15:02:28 +1100

    added read from file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,6 +7,6 @@ fn main() {
     let query = &args[1];
     let file_path = &args[2];
 
-    println!("query: {}", query);
-    println!("file_path: {}", file_path);
+    let contents = fs::read_to_string(&file_path).expect("Should be able to read file");
+    println!("contents: {}", contents);
 }
