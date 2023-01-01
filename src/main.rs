18eb1a2b5d389450e7c2ffa21b3d534c81204ffc
Laurence Qi <laurence.qi@outlook.com>
2023-01-01 15:20:01 +1100

    converted argument parsing into struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(&file_path).expect("Should be able to read file");
+    let contents = fs::read_to_string(config.file_path).expect("Should be able to read file");
     println!("contents: {}", contents);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &Vec<String>) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+    
+        Ok(Config { query, file_path })
+    }
 }
