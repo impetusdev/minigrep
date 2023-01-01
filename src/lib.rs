7a108ebf637a86573792614cbc706b5797270519
Laurence Qi <laurence.qi@outlook.com>
2023-01-01 20:27:07 +1100

    finished

--- src/lib.rs
+++ src/lib.rs
@@ -21,9 +21,23 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?; // improve this error handling around the reading of file
-    println!("contents: {}", contents);
 
+	for line in search(&config.query, &contents) {
+		println!("{line}");
+	}
+	
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+	let mut result = vec![];
+	
+	for line in contents.lines() {
+		if line.contains(query) {
+			result.push(line);
+		}
+	}
+	result
 }
 
 #[cfg(test)]
@@ -34,9 +48,9 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-        Rust: 
-        safe, fast, productive. 
-        Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
