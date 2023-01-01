d50edb50b6ecebe19e93d199010dcbd2dff51b68
Laurence Qi <laurence.qi@outlook.com>
2023-01-01 18:58:41 +1100

    extracted struct

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,21 @@
 use std::env;
-use std::fs;
 use std::process;
+use minigrep;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    let contents = fs::read_to_string(config.file_path).expect("Should be able to read file");
-    println!("contents: {}", contents);
-}
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &Vec<String>) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-    
-        Ok(Config { query, file_path })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
+
